c288a591175c92c545be593f07168bcbeae73daa
Adam Kasperczak <adamkasperczak123@gmail.com>
2020-11-26 14:28:05 +0100

    init with basic supplier entity

---
+++ src/Entity/Supplier/Supplier.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Entity\Supplier;
+
+final class Supplier implements SupplierInterface
+{
+    /** @var string|null */
+    private $id;
+
+    /** @var string|null */
+    private $name;
+
+    /** @var string|null */
+    private $email;
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+}
